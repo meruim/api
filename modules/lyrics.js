5ec281f947b6d3fc0bfd226957c440db5c492280
mangroveprot <mangroveprot@gmail.com>
2024-01-11 16:03:59 +0800

    Fixed Lyrics

--- modules/lyrics.js
+++ modules/lyrics.js
@@ -1,26 +1,42 @@
 module.exports = {
-  run: async function ({ app, fs, path, express, axios, request, ytdl, yts, requestHandler }) {
+  run: async function ({
+    app, fs, path, express, axios, request, ytdl, yts, requestHandler
+  }) {
     app.get('/api/lyrics', async (req, res) => {
       try {
-        const { song } = req.query;
+        const {
+          song
+        } = req.query;
 
         if (!song) {
-          return res.status(400).json({ error: "Missing 'song' parameter!" });
+          return res.status(400).json({
+            error: "Missing 'song' parameter!"
+          });
         }
 
-        const lyricsApiUrl = `https://api.heckerman06.repl.co/api/other/lyrics2?song=${encodeURIComponent(song)}`;
+        const lyricsApiUrl = `https://chards-api.replit.app/api/lyrics/song?title=${encodeURIComponent(song)}`;
 
         const lyricsResponse = await axios.get(lyricsApiUrl);
-        const { title, artist, lyrics, image } = lyricsResponse.data;
+        const {
+          title,
+          artist,
+          song_thumbnail,
+          lyrics,
+        } = lyricsResponse.data.content;
 
         const message = {
-          title, image, artist, lyrics
+          title,
+          image: `${song_thumbnail}`,
+          artist,
+          lyrics
         };
 
         res.json(message);
       } catch (error) {
         console.error('[ERROR]', error);
-        res.status(500).json({ error: 'Internal Server Error.' });
+        res.status(500).json({
+          error: 'Internal Server Error.'
+        });
       }
     });
   }
