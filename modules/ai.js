765c64f78190ffa6139611aa9e397c7e74432601
mangroveprot <mangroveprot@gmail.com>
2024-01-15 13:41:40 +0800

    ai question error fixed

--- modules/ai.js
+++ modules/ai.js
@@ -1,28 +1,41 @@
-module.exports ={
-  run: async function({axios, app, requestHandler}){
+module.exports = {
+  run: async function({
+    axios, app, requestHandler
+  }) {
 
-    app.get('/api/ai', async (req,res)=>{
-      let { prompt, uid, name } = req.query
+    app.get('/api/ai', async (req, res)=> {
+      let {
+        prompt, uid, name
+      } = req.query
 
       if (!prompt || !uid) {
-        return res.status(400).json({ error: "Missing 'prompt' and 'uid' parameter!" });
+        return res.status(400).json({
+          error: "Missing 'prompt' and 'uid' parameter!"
+        });
       }
 
-      const handle = await requestHandler.userRequest(uid, name );
+      const handle = await requestHandler.userRequest(uid, name);
       if (handle) {
-        return res.json({result: handle.error});
+        return res.json({
+          result: handle.error
+        });
       }
-      
+
       try {
-       const response = await axios.get(`https://hercai.onrender.com/gemini/hercai?question=${encodeURIComponent(prompt)}`);
+        const response = await axios.get(`https://hercai.onrender.com/gemini/hercai?question=${encodeURIComponent(prompt)}`);
         if (response.data) {
-          const {reply} = response.data;
-          if(!reply) return;
-          res.json({ result: reply });
+          const {
+            reply
+          } = response.data;
+          if (!reply) return;
+          if (reply == "Question Error!") return;
+          res.json({
+            result: reply
+          });
           return;
         }
       } catch (error) {
-         console.error("Error:", error.message);
+        console.error("Error:", error.message);
       }
 
       try {
@@ -32,10 +45,11 @@ module.exports ={
           const result = response.data.response;
           const textToRemove = "\n\nIs this answer helpful to you? Kindly click the link below\nhttps://click2donate.kenliejugarap.com\n(Clicking the link and clicking any ads or button and wait for 30 seconds (3 times) everyday is a big donation and help to us to maintain the servers, last longer, and upgrade servers in the future)";
           const cleanedResult = result && result.endsWith(textToRemove)
-            ? result.slice(0, -textToRemove.length)
-            : result;
+          ? result.slice(0, -textToRemove.length): result;
 
-          res.json({ result: cleanedResult });
+          res.json({
+            result: cleanedResult
+          });
           return;
         } else {
           console.error(error);
@@ -45,20 +59,26 @@ module.exports ={
       }
 
       try {
-       const response = await axios.get(`https://api.easy-api.online/v1/globalgpt?q=${encodeURIComponent(prompt)}`);
+        const response = await axios.get(`https://api.easy-api.online/v1/globalgpt?q=${encodeURIComponent(prompt)}`);
         if (response.data) {
-          const {content} = response.data;
-          res.json({ result: content });
+          const {
+            content
+          } = response.data;
+          res.json({
+            result: content
+          });
           return;
         }
       } catch (error) {
-         console.error("Error:", error.message);
+        console.error("Error:", error.message);
       }
 
 
 
-     
-      res.status(500).json({ result: "Internal Server Error" });
+
+      res.status(500).json({
+        result: "Internal Server Error"
+      });
     });
   },
 };
