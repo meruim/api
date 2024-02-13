e16342a936d8f9bf8926bb2b72e35dba1ee7f0a6
mangroveprot <mangroveprot@gmail.com>
2024-02-13 22:00:53 +0800

    Fix ai

--- modules/ai.js
+++ modules/ai.js
@@ -22,25 +22,22 @@ module.exports = {
       }
 
       try {
-        const response = await axios.get(`https://gpt.aliestercrowley.com/api?prompt=${encodeURIComponent(prompt)}`);
-        if (response.data) {
-          const {
-            message
-          } = response.data;
-          res.json({
-            result: message
-          });
-          return;
-        }
-      } catch (error) {
-        console.error("Error:", error.message);
-      }
+    const response = await axios.get(`https://api-samir.onrender.com/gpt?content=${encodeURIComponent(prompt)}`);
+    if (response.data && response.data.message && response.data.message.content) {
+        const { content } = response.data.message;
+        res.json({ result: content });
+        return;
+    }
+} catch (error) {
+    console.error("Error:", error.message);
+}
 
-      try {
+      /*  try {
         const response = await axios.get(`https://api.kenliejugarap.com/ai/?text=${encodeURIComponent(prompt)}`);
 
         if (response.data && response.data.response) {
           const result = response.data.response;
+          if (result === "Failed to fetch nonce value.") return;
           const textToRemove = "\n\nIs this answer helpful to you? Kindly click the link below\nhttps://click2donate.kenliejugarap.com\n(Clicking the link and clicking any ads or button and wait for 30 seconds (3 times) everyday is a big donation and help to us to maintain the servers, last longer, and upgrade servers in the future)";
           const cleanedResult = result && result.endsWith(textToRemove)
           ? result.slice(0, -textToRemove.length): result;
@@ -55,7 +52,7 @@ module.exports = {
       } catch (error) {
         console.error("Error:", error.message);
       }
-
+     */
       try {
         const response = await axios.get(`https://api.easy-api.online/v1/globalgpt?q=${encodeURIComponent(prompt)}`);
         if (response.data) {
